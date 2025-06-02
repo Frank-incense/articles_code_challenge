28aa4e742d213e11fa9848977e47af0d5f9e5c1d
Frankincense <frankincensewelsey@gmail.com>
2025-06-02 22:16:22 +0300

    still working

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,5 +1,5 @@
 import sqlite3
+
 def get_connection():
     db_conn = sqlite3.connect('code_challenge.db')
-    db_conn.row_factory = sqlite3.Row
     return db_conn
--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,21 +1,20 @@
-
 CREATE TABLE IF NOT EXISTS articles(
-    id INTEGER PRIMARY KEY,
-    author_id INTEGER NOT NULL,
-    magazine_id INTEGER NOT NULL,
-    title VARCHAR NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES authors(id),
-    FOREIGN KEY (magazine_id) REFERENCES mgazines(id)
-    );
+id INTEGER PRIMARY KEY,
+author_id INTEGER NOT NULL,
+magazine_id INTEGER NOT NULL,
+title VARCHAR NOT NULL,
+FOREIGN KEY (author_id) REFERENCES authors(id),
+FOREIGN KEY (magazine_id) REFERENCES magazines(id)
+);
 
 CREATE TABLE IF NOT EXISTS authors(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL
-    );
+id INTEGER PRIMARY KEY,
+name VARCHAR NOT NULL
+);
 
 
-CREATE TABLE magazines(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR,
-    category VARCHAR
-    );
+CREATE TABLE IF NOT EXISTS magazines(
+id INTEGER PRIMARY KEY,
+name VARCHAR,
+category VARCHAR
+);
